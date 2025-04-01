2044386322acd325e69040c21acd83b3929b83d5
Seshu Sai <105602908+iseshu@users.noreply.github.com>
2025-04-02 03:04:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from os import environ as env
-
+from dotenv import load_dotenv
 class Telegram:
     API_ID = int(env.get("API_ID"))
     API_HASH = env.get("API_HASH")
@@ -11,3 +11,4 @@ class Telegram:
     FILTER_AMAZON_TAGS = [chat_id for chat_id in env.get("FILTER_AMAZON_TAGS").split(',')]
     YOUR_AMAZON_TAG = env.get('YOUR_AMAZON_TAG')
     EXTRAPE_SESSION_SECRET = env.get('EXTRAPE_SESSION_SECRET')
+    LOG_GROUP_ID = int(env.get("LOG_GROUP_ID"))
